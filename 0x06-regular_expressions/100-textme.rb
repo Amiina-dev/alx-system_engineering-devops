2a62a672a08d72a50946830d506252dc41fee087
Amab <miynaoje@outlook.com>
2023-01-31 18:29:19 -0800

    Made exec

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # parse logfile and output [sender], [receiver], [flags]
 
-puts ARGV[0].scan(/\[?:from:|to:|flags:)(.*?)\]/).join(",")
+puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
