7a071e61e2a04e4a2e3272eb1da6c4150bb0a1cd
Amab <miynaoje@outlook.com>
2023-01-31 18:07:09 -0800

    Corrections

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,4 @@
 #!/usr/bin/env ruby
 # A ruby script that uses a regex that must match school
+
 puts ARGV[0].scan(/School/).join
--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,2 +1,4 @@
 #!/usr/bin/env ruby
+# match a given pattern
+
 puts ARGV[0].scan(/hbt{2,5}n/).join
